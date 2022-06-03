16ef7b2b1d791b32e8fe23030f063d00b5fe62c0
Ancash <bdspark2005@gmail.com>
2022-06-03 18:15:19 +0200

    3.4.1

--- ILibrary/src/de/ancash/ILibrary.java
+++ ILibrary/src/de/ancash/ILibrary.java
@@ -38,7 +38,7 @@ public class ILibrary extends JavaPlugin{
 		plugin = this;
 		f = new YamlFile(new File("plugins/ILibrary/config.yml"));
 		try {
-			//asd
+			//asdasd
 			if(!f.exists()) 
 				FileUtils.copyInputStreamToFile(getResource("config.yml"), new File(f.getFilePath()));
 			
