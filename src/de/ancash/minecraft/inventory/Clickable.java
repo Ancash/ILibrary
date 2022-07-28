ba0df68df6e35111751ff67cc44668a676cfae88
Ancash <bdspark2005@gmail.com>
2022-07-28 19:27:29 +0200

    a

--- src/de/ancash/minecraft/inventory/Clickable.java
+++
@@ -1,9 +0,0 @@
-package de.ancash.minecraft.inventory;
-
-import org.bukkit.event.inventory.InventoryAction;
-
-public abstract interface Clickable {
-
-	void onClick(int slot, boolean shift, InventoryAction action, boolean topInventory);
-	
-}	
