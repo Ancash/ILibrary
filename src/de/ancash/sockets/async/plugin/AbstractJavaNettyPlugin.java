837c31b00737fbd59d40039ae786e9e253ab6abf
denzo <denzo@ryzen7700>
2025-11-29 20:42:47 +0100

    Merge branch 'master' of https://github.com/Ancash/ILibrary.git

---
+++ src/de/ancash/sockets/async/plugin/AbstractJavaNettyPlugin.java
@@ -0,0 +1,48 @@
+package de.ancash.sockets.async.plugin;
+
+import java.util.UUID;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+import de.ancash.sockets.async.impl.packet.netty.NettyPacketClient;
+import de.ancash.sockets.packet.Packet;
+import de.ancash.sockets.packet.PacketFuture;
+
+public abstract class AbstractJavaNettyPlugin extends JavaPlugin {
+
+	static AtomicInteger cnt = new AtomicInteger();
+
+	protected NettyPacketClient client;
+
+	public synchronized boolean connect(String address, int port) {
+
+		if (client != null) {
+			client.disconnect();
+			client = null;
+		}
+		try {
+			client = new NettyPacketClient(address, port, this::onPacketReceive, this::onClientConnect, this::onClientDisconnect);
+			client.connect();
+			return client.isConnected();
+		} catch (InterruptedException e) {
+			getLogger().severe("Could not connect to " + address + ":" + port + ": " + e);
+			return false;
+		}
+	}
+
+	public PacketFuture sendPacket(Packet packet) {
+		return sendPacket(packet, null);
+	}
+
+	public PacketFuture sendPacket(Packet packet, UUID uuid) {
+		client.write(packet);
+		return new PacketFuture(packet, uuid);
+	}
+
+	public abstract void onClientDisconnect();
+
+	public abstract void onClientConnect();
+
+	public abstract void onPacketReceive(Packet packet);
+}
